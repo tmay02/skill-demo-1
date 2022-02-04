0100a35339b2e836bb42a70d1e7647d0119ae2b1
tmay02 <97641235+tmay02@users.noreply.github.com>
2022-02-04 14:35:43 -0800

    test functions properly

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void testSubtract() {
-        assertEquals(1, SkillDemo.subtract(3,1));
+        assertEquals(2, SkillDemo.subtract(3,1));
     }
 }
